9ceb233962f10ac16761002818a443c0c787247f
Bartłomiej Mieżyński <bartek@barmie.dev>
2023-11-05 18:55:24 +0100

    :wrench: favicons, prettier, tsconfig

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    container: {
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {},
   },
   plugins: [],
