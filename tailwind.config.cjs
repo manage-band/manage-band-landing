666636e36d1f9272acde8404778960aad37bfe01
Bartłomiej Mieżyński <bartek@barmie.dev>
2023-11-11 20:18:17 +0100

    :art: improved structure of tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,15 +3,22 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     container: {
+      center: true,
       screens: {
         '2xl': '1400px',
       },
     },
     extend: {
       colors: {
-        background: '#111115',
-        darkBackground: '#0D0D0F',
-        brandRed: '#EC213A',
+        brand: '#EC213A',
+        background: {
+          DEFAULT: '#111115',
+          dark: '#0D0D0F',
+        },
+        text: {
+          DEFAULT: '#FFFFFF',
+          muted: '#E3E3E3',
+        },
       },
     },
   },
