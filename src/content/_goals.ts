b322a8382c6975618c2799d6d7bbc21db777c9f1
Bartłomiej Mieżyński <bartek@barmie.dev>
2023-11-11 19:01:15 +0100

    :speech_balloon: updated goals content

--- src/content/_goals.ts
+++ src/content/_goals.ts
@@ -5,26 +5,26 @@ export const goals: GoalType[] = [
     title: '.goal',
     img: 'mb-goal.png',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae diam eget risus varius blandit sit amet non magna. Sed vitae diam eget risus varius blandit sit amet non magna.',
+      'Koncert? Singiel? A może cała płyta? Przeprowadzimy Cię za rękę od pomysłu aż do realizacji danego celu. Brakuje Ci jakichś kroków? Dodaj swoje własne i realizuj je z nami!',
     styling: 'lg:col-span-2',
   },
   {
     title: '.contact',
     img: 'mb-goal.png',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae diam eget risus varius blandit sit amet non magna. Sed vitae diam eget risus varius blandit sit amet non magna.',
+      'Przejmij kontrolę nad swoją karierą. Zarządzaj kontaktami z wytwórniami, managerami, producentami, dziennikarzami i innymi osobami, które pomogą Ci w realizacji Twoich celów.',
   },
   {
     title: '.storage',
     img: 'mb-goal.png',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae diam eget risus varius blandit sit amet non magna. Sed vitae diam eget risus varius blandit sit amet non magna.',
+      'Bałagan w plikach? Nie u nas. Przechowuj swoje utwory, teksty, zdjęcia i inne pliki w jednym miejscu. Dzięki temu zawsze będziesz mieć do nich łatwy dostęp.',
   },
   {
     title: '.artist',
     img: 'mb-goal.png',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae diam eget risus varius blandit sit amet non magna. Sed vitae diam eget risus varius blandit sit amet non magna.',
+      'Znowu zapomniałeś o poście na Instagramie? Nie martw się, my Ci przypomnimy. Miej zawsze pod ręką wszystkie materiały dotyczące zbliającej się premiery lub koncertu. Zachowaj konsekwencję w działaniach i zyskaj nowych fanów!',
     styling: 'lg:col-span-2',
   },
 ];
