dfd7f168f13376c9927ce8a561806dac60dbef9a
Bartłomiej Mieżyński <bartek@barmie.dev>
2023-11-12 15:22:54 +0100

    :lipstick: changed icons and hero background

--- src/content/_goals.ts
+++ src/content/_goals.ts
@@ -3,26 +3,26 @@ import type { Goal } from '@/types';
 export const goals: Goal[] = [
   {
     title: '.goal',
-    img: 'mb-goal.png',
+    img: 'goal-icon.svg',
     description:
       'Koncert? Singiel? A może cała płyta? Przeprowadzimy Cię za rękę od pomysłu aż do realizacji danego celu. Brakuje Ci jakichś kroków? Dodaj swoje własne i realizuj je z nami!',
     styling: 'lg:col-span-2',
   },
   {
     title: '.contact',
-    img: 'mb-peers.png',
+    img: 'contact-icon.svg',
     description:
       'Przejmij kontrolę nad swoją karierą. Zarządzaj kontaktami z wytwórniami, managerami, producentami, dziennikarzami i innymi osobami, które pomogą Ci w realizacji Twoich celów.',
   },
   {
     title: '.storage',
-    img: 'mb-pc.png',
+    img: 'storage-icon.svg',
     description:
       'Bałagan w plikach? Nie u nas. Przechowuj swoje utwory, teksty, zdjęcia i inne pliki w jednym miejscu. Dzięki temu zawsze będziesz mieć do nich łatwy dostęp.',
   },
   {
     title: '.artist',
-    img: 'mb-pc.png',
+    img: 'artist-icon.svg',
     description:
       'Znowu zapomniałeś o poście na Instagramie? Nie martw się, my Ci przypomnimy. Miej zawsze pod ręką wszystkie materiały dotyczące zbliającej się premiery lub koncertu. Zachowaj konsekwencję w działaniach i zyskaj nowych fanów!',
     styling: 'lg:col-span-2',
